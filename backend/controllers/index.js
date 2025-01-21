de2a8f34b0df3c970cfb4723d78acd162482c4f3
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-21 16:51:52 +0100

    gather all the controller in one file

---
+++ backend/controllers/index.js
@@ -0,0 +1,5 @@
+const signup = require('../controllers/signup');
+const login = require('../controllers/login');
+const logout = require('../controllers/logout');
+
+module.exports = { signup, login, logout };
