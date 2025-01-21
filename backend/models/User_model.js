75574c9c8ee6de6a375148fc8cfbd971ea4b760a
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-21 14:33:22 +0100

    user model

---
+++ backend/models/User_model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', UserSchema);
