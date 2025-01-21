73fa9b25ab88ead17022061054dbc0cd2cdb982b
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-21 15:09:09 +0100

    add PORT parameter  to the app.listen function

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(() => {
+app.listen(PORT, () => {
   connectDB();
   console.log('Server is running on port: ', PORT);
 });
