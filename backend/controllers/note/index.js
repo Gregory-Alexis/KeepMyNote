139815fa6a72e682a68e0b5b31c5965db1bff5eb
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-22 17:31:01 +0100

    gather all note controllers in one file

---
+++ backend/controllers/note/index.js
@@ -0,0 +1,6 @@
+const getNotes = require('./get_note');
+const createNote = require('./create_note');
+const deleteNote = require('./delete_note');
+const updateNote = require('./update_note');
+
+module.exports = { getNotes, createNote, deleteNote, updateNote };
