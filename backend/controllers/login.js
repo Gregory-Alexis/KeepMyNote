b023a5fd61cf795477537eac70da3ebf00180f22
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-22 15:48:16 +0100

    add await keyword, return rhe response to exit the process if an error occur & change the pwd in the res null to undefined

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,30 +1,28 @@
-const { get } = require('mongoose');
-const User = require('../models/User_model');
 const bcrypt = require('bcrypt');
+
+const User = require('../models/User_model');
 const generateTokenAndSetCookie = require('../utils/generateAndSetCookie');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = User.findOne({ email });
+    const user = await User.findOne({ email });
     if (!user) {
-      res.status(400).json({ success: false, message: 'Invalid credentials' });
+      return res.status(400).json({ success: false, message: 'Invalid credentials' });
     }
 
-    const isPasswordValid = bcrypt.compare(password, user.password);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      res.status(400).json({ success: false, message: 'Invalid credentials' });
+      return res.status(400).json({ success: false, message: 'Invalid credentials' });
     }
 
-    generateTokenAndSetCookie(req, user._id);
-
-    await user.save;
+    generateTokenAndSetCookie(res, user._id);
 
     res.status(200).json({
       success: true,
       message: 'Logged in successfully',
-      user: { ...user._doc, password: null },
+      user: { ...user._doc, password: undefined },
     });
   } catch (error) {
     console.log('Error in login', error);
