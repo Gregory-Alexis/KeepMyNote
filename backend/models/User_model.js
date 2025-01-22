919864b63ad007a56c57bb6a7f844daa6d352cce
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-22 17:36:16 +0100

    change the names to username and made the relation between the user and the note he create

--- backend/models/User_model.js
+++ backend/models/User_model.js
@@ -1,28 +1,10 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  notes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Note' }],
+});
 
-const UserSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
