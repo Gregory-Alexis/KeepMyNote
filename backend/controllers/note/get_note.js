00be13875977f2646d40de03625b50a124555722
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-23 14:18:06 +0100

    controller to get all the notes

---
+++ backend/controllers/note/get_note.js
@@ -0,0 +1,12 @@
+const Note = require('../../models/Note_model');
+
+const getNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json({ success: true, notes });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = getNotes;
