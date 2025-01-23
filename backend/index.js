1496e530218e69337d5ac6a3df9c43656729ad8b
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-23 15:47:10 +0100

    add cors

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
