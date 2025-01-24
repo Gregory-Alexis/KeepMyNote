5cf8b47a5b48630f45996b1e007c8a52078b00b4
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-24 16:27:28 +0100

    HomePage renders form to add a note and the cards

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import CardNote from '../components/CardNote';
+import FormInput from '../components/FormInput';
+
+const HomePage = () => {
+  return (
+    <>
+      <div className='flex justify-center'>
+        <FormInput />
+      </div>
+      <CardNote />
+    </>
+  );
+};
+export default HomePage;
