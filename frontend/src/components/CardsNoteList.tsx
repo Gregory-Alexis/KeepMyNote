46a10f92d62560cf9ff9e386a983996352ce1dce
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-24 16:26:53 +0100

    List of notes component

---
+++ frontend/src/components/CardsNoteList.tsx
@@ -0,0 +1,6 @@
+const CardsNoteList = () => {
+  return (
+    <div className='w-lg h-80 m-12 rounded-2xl shadow-[3px_3px_10px_5px_rgba(31,41,55,0.50)]'></div>
+  );
+};
+export default CardsNoteList;
