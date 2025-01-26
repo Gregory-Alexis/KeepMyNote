a04b7ebba1bb964e1e15518445c88f4b0f4e2bba
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 15:49:40 +0100

    remove "notes" from the object response so it stays an array of object

--- backend/controllers/note/get_note.js
+++ backend/controllers/note/get_note.js
@@ -3,7 +3,7 @@ const Note = require('../../models/Note_model');
 const getNotes = async (req, res) => {
   try {
     const notes = await Note.find();
-    res.status(200).json({ success: true, notes });
+    res.status(200).json(notes);
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
