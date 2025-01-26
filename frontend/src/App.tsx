bada3ed7e0437c5c6222584918431f56477daf8a
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 12:04:32 +0100

    add checkAuth to verify is a user is connected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
+import { useAuthStore } from './store/auth_store';
+import { useEffect } from 'react';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const { checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
