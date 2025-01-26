007c4285aa2ee68766aace984275d7d86a8b2bac
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 12:06:19 +0100

    add type ofr login, logout & checkAuth

--- frontend/src/models/AuthStore.ts
+++ frontend/src/models/AuthStore.ts
@@ -7,4 +7,7 @@ export type AuthStore = {
   error: string | null;
   isAuthChecked: boolean;
   signup: (username: string, email: string, password: string) => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  checkAuth: () => Promise<void>;
 };
