4af851f6c38d56da33a7d80702c202a83e077254
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 15:52:08 +0100

    Form input component that handles title and content

--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -1,24 +1,64 @@
+import { useForm } from 'react-hook-form';
+import { useAddNote } from '../mutations/addNote';
+import { Note, FormData } from '../models/Note';
+import { useAuthStore } from '../store/auth_store';
+
 const FormInput = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>();
+  const mutation = useAddNote();
+  const { user } = useAuthStore();
+
+  const onSubmit = (data: FormData) => {
+    const newNote: Note = {
+      _id: Date.now().toString(),
+      title: data.title,
+      content: data.note,
+      userID: user?.id,
+    };
+
+    mutation.mutate(newNote, {
+      onSuccess: () => {
+        reset(); // Reset the form after successful submission
+      },
+      onError: (error) => {
+        console.error('Failed to add note:', error);
+      },
+    });
+  };
+
   return (
-    <form className='mt-8 rounded-2xl shadow-[3px_3px_10px_5px_rgba(31,41,55,0.50)] xl:w-2/8 '>
-      <div className='flex flex-col items-center w-full  bg-white p-5 rounded-lg relative'>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className='mt-8 rounded-2xl shadow-[3px_3px_10px_5px_rgba(31,41,55,0.50)] xl:w-2/8'
+    >
+      <div className='flex flex-col items-center w-full bg-white p-5 rounded-lg relative'>
         <input
           placeholder='Title'
-          name='title'
           type='text'
           aria-label='Title'
-          value={''}
           className='outline-none w-full mb-2 placeholder:text-gray-500 font-semibold border-b-1'
+          {...register('title', {
+            required: true,
+            maxLength: { value: 50, message: 'The title cannot be longer than 50 characters' },
+          })}
         />
-
+        {errors.title && <p className='text-red-500'>{errors.title.message}</p>}
         <textarea
           placeholder='Write your note...'
-          name='note'
           aria-label='Write your note...'
-          value={''}
-          className='outline-none  w-full h-24 cursor-auto font-light placeholder:text-gray-500 placeholder:font-semibold'
+          className='outline-none w-full h-24 cursor-auto font-light placeholder:text-gray-500 placeholder:font-semibold'
           style={{ resize: 'none' }}
+          {...register('note', {
+            required: true,
+            maxLength: { value: 200, message: 'The note cannot be longer than 200 characters' },
+          })}
         />
+        {errors.note && <p className='text-red-500'>{errors.note.message}</p>}
         <button
           type='submit'
           className='bg-gray-800 text-white px-4 py-2 rounded-full absolute bottom-2 right-2'
@@ -29,4 +69,5 @@ const FormInput = () => {
     </form>
   );
 };
+
 export default FormInput;
