c788f13f935e86a7e9f6ea3dfd0a16a6b479f0bf
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 12:08:34 +0100

    protect the routes depending if a user is connected or not

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -4,13 +4,36 @@ import Layout from '../layout/Layout';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import SignUpPage from '../pages/SignUpPage';
+import { ProtectedRoute } from '../components/ProtectedRoute';
+import RedirectAuthenticatedUser from '../components/RedirectAuthenticatedUser';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/signup' element={<SignUpPage />} />
+      <Route
+        index
+        element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/login'
+        element={
+          <RedirectAuthenticatedUser>
+            <LoginPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path='/signup'
+        element={
+          <RedirectAuthenticatedUser>
+            <SignUpPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
     </Route>
   )
 );
