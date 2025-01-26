755ec2b00edfcfbdfd29ac9407bd67cf882eabae
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 15:50:39 +0100

    CardNote component renders the list of notes

--- frontend/src/components/CardNote.tsx
+++ frontend/src/components/CardNote.tsx
@@ -1,9 +1,22 @@
+import { useNotes } from '../queries/fetchNotes';
 import CardsNoteList from './CardsNoteList';
 
 const CardNote = () => {
+  const { data: notes, isLoading, isError } = useNotes();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error loading notes</div>;
+  }
+
   return (
-    <div>
-      <CardsNoteList />
+    <div className='flex flex-wrap justify-center'>
+      {notes?.map((note) => (
+        <CardsNoteList key={note._id} title={note.title} content={note.content} />
+      ))}
     </div>
   );
 };
