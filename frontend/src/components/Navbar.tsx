07b0e4525c031bb42a8fea8a3e686651320b289a
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 13:25:40 +0100

    add the logout function & change the ui depending if a user is connected or not

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,23 +2,43 @@ import { Link } from 'react-router-dom';
 import { useAuthStore } from '../store/auth_store';
 
 const Navbar = () => {
-  const { isAuthenticated, user } = useAuthStore();
+  const { isAuthenticated, user, logout } = useAuthStore();
+
+  const handleLogout = async () => {
+    await logout();
+  };
   return (
     <nav className='bg-gray-800 text-white flex justify-between items-center p-4'>
       {isAuthenticated && <span className='text-2xl'>{user?.username}</span>}
 
       <span className=' text-2xl font-bold'>Keep My Notes</span>
 
-      <ul className='flex justify-end space-x-4 '>
-        <li className='bg-gray-700 px-4 py-2 rounded  transform transition duration-300 hover:bg-gray-500'>
-          <Link to='/login'>{isAuthenticated ? 'Logout' : 'Login'}</Link>
-        </li>
-        {!isAuthenticated && (
-          <li className='bg-gray-700 px-4 py-2 rounded transform transition duration-300 hover:bg-gray-500'>
-            <Link to='/signup'>Signup</Link>
-          </li>
+      <div className='flex justify-end space-x-4 '>
+        {!isAuthenticated ? (
+          <Link
+            to='login'
+            className='bg-gray-700 px-4 py-2 rounded  transform transition duration-300 hover:bg-gray-500'
+          >
+            Login
+          </Link>
+        ) : (
+          <button
+            onClick={handleLogout}
+            className='bg-gray-700 px-4 py-2 rounded  transform transition duration-300 hover:bg-gray-500'
+          >
+            Logout
+          </button>
         )}
-      </ul>
+
+        {!isAuthenticated && (
+          <Link
+            className='bg-gray-700 px-4 py-2 rounded transform transition duration-300 hover:bg-gray-500'
+            to='/signup'
+          >
+            Signup
+          </Link>
+        )}
+      </div>
     </nav>
   );
 };
