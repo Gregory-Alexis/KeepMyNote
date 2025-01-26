f8a2ba40a835f5fb2b6f8de8fbcd8a1850affeac
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 15:52:41 +0100

    formData model

--- frontend/src/models/Note.ts
+++ frontend/src/models/Note.ts
@@ -1,12 +1,17 @@
 export type Note = {
-  id: string;
+  _id: string;
   title: string;
   content: string;
-  userID: string;
+  userID: string | undefined;
 };
 
 // Define a context type to store the previous notes
 export type MutationContext = {
   // Store the previous notes so we can roll back if there is an error
   previousNotes?: Note[];
+};
+
+export type FormData = {
+  title: string;
+  note: string;
 };
