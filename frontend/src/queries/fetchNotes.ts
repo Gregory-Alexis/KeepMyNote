1d2b51e0cde311a4bb76580f8eaa2d31a3250df7
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-26 15:53:29 +0100

    Fetch all the notes from the database

---
+++ frontend/src/queries/fetchNotes.ts
@@ -0,0 +1,28 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { Note } from '../models/Note';
+import axios from 'axios';
+
+export const fetchNotes = async (): Promise<Note[]> => {
+  const NOTE_API_URL = 'http://localhost:5000/api/notes';
+
+  const response = await axios.get(NOTE_API_URL);
+
+  const data = await response.data;
+  return data;
+};
+
+export const useNotes = (): UseQueryResult<Note[]> => {
+  return useQuery({
+    // The query key is used to cache the data
+    queryKey: ['notes'],
+    // The query function is called whenever the data is stale
+    queryFn: fetchNotes,
+
+    // Refetch the data every 10 seconds
+    staleTime: 10000,
+    //cacheTime: 60000,
+
+    // Refetch the data every time the window regains focus
+    refetchOnWindowFocus: true,
+  });
+};
