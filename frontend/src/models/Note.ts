45c17a26d34cc2cce8a40ae04dc9165018a70dfd
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-27 13:18:50 +0100

    add a NewNote model

--- frontend/src/models/Note.ts
+++ frontend/src/models/Note.ts
@@ -5,6 +5,13 @@ export type Note = {
   userID: string | undefined;
 };
 
+export type NewNote = {
+  _id: string;
+  title: string;
+  content: string;
+  userID: string | undefined;
+};
+
 // Define a context type to store the previous notes
 export type MutationContext = {
   // Store the previous notes so we can roll back if there is an error
