440312305319d2a31dc65920338e19fd007fbc16
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-01-27 17:39:30 +0100

    remove protectedRoute component

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -4,20 +4,12 @@ import Layout from '../layout/Layout';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import SignUpPage from '../pages/SignUpPage';
-import { ProtectedRoute } from '../components/ProtectedRoute';
 import RedirectAuthenticatedUser from '../components/RedirectAuthenticatedUser';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-      <Route
-        index
-        element={
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        }
-      />
+      <Route index element={<HomePage />} />
       <Route
         path='/login'
         element={
