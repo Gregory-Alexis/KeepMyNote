6100c47be0aec6e4d5d159f5a0763e48e4648d2d
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-01 11:26:35 +0100

    delete the user and his notes

--- backend/controllers/user/deleteUser.js
+++ backend/controllers/user/deleteUser.js
@@ -1,4 +1,5 @@
 const User = require('../../models/User_model');
+const Note = require('../../models/Note_model'); // Assurez-vous d'importer le modèle des notes
 
 const deleteUser = async (req, res) => {
   try {
@@ -17,11 +18,15 @@ const deleteUser = async (req, res) => {
       });
     }
 
+    await Note.deleteMany({ user: userID });
+
     await User.findByIdAndDelete(userID);
 
     res.clearCookie('token', { httpOnly: true, secure: true, sameSite: 'Strict' });
 
-    res.status(200).json({ success: true, message: 'User deleted successfully' });
+    res
+      .status(200)
+      .json({ success: true, message: 'User and associated notes deleted successfully' });
   } catch (error) {
     console.error('Error in deleteUser:', error);
     res.status(500).json({ success: false, message: 'Internal server error' });
