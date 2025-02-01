1bfb1849c2f483f91e732ae8785d5e1d89948291
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-01 09:22:41 +0100

    add logic to change navbar depending on the window width

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,12 +1,35 @@
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../components/Navbar';
+import MobileNavbar from '../components/MobileNavbar';
 import Footer from '../components/Footer';
+import { useEffect, useState } from 'react';
 
 const Layout = () => {
+  const [toggle, setToggle] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleToggleButton = () => {
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setWidth(window.innerWidth);
+      if (window.innerWidth >= 1024) {
+        setToggle(false);
+      }
+    };
+    window.addEventListener('resize', changeWidth);
+    return () => {
+      window.removeEventListener('resize', changeWidth);
+    };
+  }, []);
+
   return (
     <>
-      <Navbar />
+      <Navbar width={width} toggle={toggle} handleToggleButton={handleToggleButton} />
+      {toggle && <MobileNavbar />}
       <main className='min-h-screen bg-gray-200'>
         <Outlet />
       </main>
